35d09a9a7f0990876e540d730928b6844a37fc0b
ahmedessam3le <ahmedessam3le@gmail.com>
2023-02-25 19:01:44 +0200

    feat: Add Ai Handler

--- lib/services/ai_handler.dart
+++ lib/services/ai_handler.dart
@@ -1,5 +1,30 @@
+import 'dart:developer';
+
+import 'package:chat_gpt/constants/api_key.dart';
 import 'package:chat_gpt_sdk/chat_gpt_sdk.dart';
 
 class AiHandler {
-  final _openAI = OpenAI.instance.build();
+  final _openAI = OpenAI.instance.build(
+    token: apiKey,
+    baseOption: HttpSetup(receiveTimeout: 20000),
+  );
+
+  Future<String> ask(String message) async {
+    try {
+      final request = CompleteText(prompt: message, model: kTranslateModelV3);
+      final response = await _openAI.onCompleteText(request: request);
+
+      if (response != null) {
+        return response.choices.first.text.trim();
+      }
+      return 'Something went wrong';
+    } catch (error) {
+      log('-------------------- ask ERROR --------------------\n${error.toString()}');
+      return 'Bad response';
+    }
+  }
+
+  void dispose() {
+    _openAI.close();
+  }
 }
